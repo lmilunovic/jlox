f5d9142c726754b70b695bddad0ae48c46b00d83
Ladislav Milunović <22495351+lmilunovic@users.noreply.github.com>
2020-02-23 19:05:06 +0100

    Added support for identifiers and keywords to scanner

--- jlox/src/main/java/info/ladislav/jlox/Scanner.java
+++ jlox/src/main/java/info/ladislav/jlox/Scanner.java
@@ -10,6 +10,28 @@ import info.ladislav.jlox.Token;
 
 class Scanner {            
 
+  private static final Map<String, TokenType> keywords;
+
+  static {                                             
+    keywords = new HashMap<>();                        
+    keywords.put("and",    AND);                       
+    keywords.put("class",  CLASS);                     
+    keywords.put("else",   ELSE);                      
+    keywords.put("false",  FALSE);                     
+    keywords.put("for",    FOR);                       
+    keywords.put("fun",    FUN);                       
+    keywords.put("if",     IF);                        
+    keywords.put("nil",    NIL);                       
+    keywords.put("or",     OR);                        
+    keywords.put("print",  PRINT);                     
+    keywords.put("return", RETURN);                    
+    keywords.put("super",  SUPER);                     
+    keywords.put("this",   THIS);                      
+    keywords.put("true",   TRUE);                      
+    keywords.put("var",    VAR);                       
+    keywords.put("while",  WHILE);                     
+  }                                                    
+
   private final String source;                                            
   private final List<Token> tokens = new ArrayList<>();                   
 
@@ -80,7 +102,9 @@ class Scanner {
         default:
           if(Character.isDigit(c)){
             number();
-          }else{
+          }else if (isAlpha(c)){
+            identifier();
+          } else{
             JLox.error(line, "Unexpected character");
           }
         break;
@@ -153,6 +177,31 @@ class Scanner {
 
     addToken(NUMBER,                                          
         Double.parseDouble(source.substring(start, current)));
-  }                                                           
+  }
+  
+  private void identifier() {
+    while (isAlphaNumeric(peek())) advance();
+
+    String text = source.substring(start, current);
+
+    TokenType type = keywords.get(text);           
+    
+    if (type == null){
+      type = IDENTIFIER;
+    }
+
+    addToken(type);                                            
+  }       
+
+  
+  private boolean isAlpha(char c) {       
+    return (c >= 'a' && c <= 'z') ||      
+           (c >= 'A' && c <= 'Z') ||      
+            c == '_';                     
+  }
+
+  private boolean isAlphaNumeric(char c) {
+    return isAlpha(c) || Character.isDigit(c);      
+  }                             
 
 }                                       
