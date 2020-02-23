a76d9a2d7418168923dd6b248757190c9f387e32
Ladislav Milunović <22495351+lmilunovic@users.noreply.github.com>
2020-02-23 18:24:57 +0100

    Added support for 2 character lexemes

--- jlox/src/main/java/info/ladislav/jlox/Scanner.java
+++ jlox/src/main/java/info/ladislav/jlox/Scanner.java
@@ -49,6 +49,12 @@ class Scanner {
       case '+': addToken(PLUS); break;           
       case ';': addToken(SEMICOLON); break;      
       case '*': addToken(STAR); break; 
+
+      case '!': addToken(match('=') ? BANG_EQUAL : BANG); break;      
+      case '=': addToken(match('=') ? EQUAL_EQUAL : EQUAL); break;    
+      case '<': addToken(match('=') ? LESS_EQUAL : LESS); break;      
+      case '>': addToken(match('=') ? GREATER_EQUAL : GREATER); break;
+      
       default:
         JLox.error(line, "Unexpected character");
         break;
@@ -67,5 +73,14 @@ class Scanner {
   private void addToken(TokenType type, Object literal) {
     String text = source.substring(start, current);      
     tokens.add(new Token(type, text, literal, line));    
-  }                                                    
+  }            
+  
+  private boolean match(char expected) {                 
+    if (isAtEnd()) return false;                         
+    if (source.charAt(current) != expected) return false;
+
+    current++;                                           
+    return true;                                         
+  }                       
+  
 }                                       
