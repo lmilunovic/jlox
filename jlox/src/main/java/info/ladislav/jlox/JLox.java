f89ec12fe7e734b1103323ccde2eb7a33c6b8399
Ladislav Milunović <22495351+lmilunovic@users.noreply.github.com>
2020-02-23 15:09:50 +0100

    Initial commit, mvn quickstart archetype

---
+++ jlox/src/main/java/info/ladislav/jlox/JLox.java
@@ -0,0 +1,13 @@
+package info.ladislav.jlox;
+
+/**
+ * Hello world!
+ *
+ */
+public class JLox 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
