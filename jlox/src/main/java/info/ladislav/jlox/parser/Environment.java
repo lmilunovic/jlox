75634e4fd2a68717c2b1a427f1ef546b11739fd9
Ladislav Milunović <milunovic.ladislav@gmail.com>
2020-04-21 21:46:12 +0200

    added suports for scope in environment class. assign method bug fix.

--- jlox/src/main/java/info/ladislav/jlox/parser/Environment.java
+++ jlox/src/main/java/info/ladislav/jlox/parser/Environment.java
@@ -7,13 +7,24 @@ import info.ladislav.jlox.lexer.Token;
 
 public class Environment {
 
+    final Environment enclosing;
     private final Map<String, Object> values = new HashMap<>();
+
+    Environment() {
+        enclosing = null;
+    }
+
+    Environment(Environment enclosing){
+        this.enclosing = enclosing;
+    }
 
     Object get(Token name){
         
         if(values.containsKey(name.lexeme)){
             return values.get(name.lexeme);
         }
+
+        if (enclosing != null) return enclosing.get(name);
 
         throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
     }
@@ -26,6 +37,12 @@ public class Environment {
 
         if(values.containsKey(name.lexeme)){
             values.put(name.lexeme, value);
+            return;
+        }
+
+        if(enclosing != null){
+            enclosing.assign(name, value);
+            return;
         }
 
         throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
